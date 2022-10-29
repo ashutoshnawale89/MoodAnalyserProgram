727418eafa86dfbb8745da0a0e2e1bfa09ff50bb
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-29 08:16:04 +0530

    Added: Print The msg

---
+++ src/main/java/com/moodanalzrProgram/MoodAnalyserProgram.java
@@ -0,0 +1,15 @@
+package com.moodanalzrProgram;
+
+import java.util.Scanner;
+
+public class MoodAnalyserProgram {
+	public String mood() {
+	return "Welcome To The Mood Analyaser";
+	}
+	public static void main(String[] args) {
+		MoodAnalyserProgram obj=new MoodAnalyserProgram();
+		System.out.println(obj.mood());
+
+	}
+
+}
